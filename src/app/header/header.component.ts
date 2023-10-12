851de341a2825a707dea8b73ff4c6576856069a8
@yasithb <yasithb@ceyentra.com>
2023-10-12 22:39:40 +0530

    about me section started to dev

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
+  @Output() selectedNavPage = new EventEmitter<string>();
 
+  onSelectNav(navPage: string) {
+    this.selectedNavPage.emit(navPage);
+  }
 }
